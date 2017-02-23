d468a7020b8cf9a946ec98462ce749dcbc8e7513
Chris <chris@codereviewvideos.com>
2017-02-23 16:58:35 +0000

    vid-7

--- src/AppBundle/Controller/DashboardController.php
+++ src/AppBundle/Controller/DashboardController.php
@@ -22,12 +22,41 @@ class DashboardController extends Controller
      * @param Request       $request
      * @param UserInterface $customer
      * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Symfony\Component\Workflow\Exception\LogicException
      */
     public function indexAction(Request $request, UserInterface $customer)
     {
+        $workflow = $this->get('workflow.customer_signup');
+
+        if ($workflow->getMarking($customer)->has('paying_customer')) {
+            return $this->redirectToRoute('success');
+        }
+
+        if ($workflow->getMarking($customer)->has('declined')) {
+            return $this->redirectToRoute('denied');
+        }
+
         return $this->render('dashboard/index.html.twig', [
             'customer' => $customer
         ]);
+    }
+
+    /**
+     * @Route("/denied", name="denied")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function deniedAction()
+    {
+        return $this->render('dashboard/denied.html.twig');
+    }
+
+    /**
+     * @Route("/success", name="success")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function successAction()
+    {
+        return $this->render('dashboard/success.html.twig');
     }
 
     /**
@@ -82,6 +111,65 @@ class DashboardController extends Controller
 
             $this->get('logger')->error('Yikes!', ['error' => $e->getMessage()]);
 
+        }
+
+        return $this->redirectToRoute('dashboard');
+    }
+
+
+
+
+    /**
+     * @Route("/add-passport", name="add_passport")
+     */
+    public function addPassportAction()
+    {
+        return $this->render('dashboard/add-passport.html.twig');
+    }
+
+    /**
+     * @Route("/submit-passport", name="submit_passport")
+     * @Method("POST")
+     * @throws \InvalidArgumentException
+     * @throws \LogicException
+     */
+    public function submitPassportAction(UserInterface $customer)
+    {
+        try {
+            $this->get('workflow.customer_signup')->apply($customer, 'add_passport');
+
+            $this->getDoctrine()->getManager()->flush();
+
+        } catch (LogicException $e) {
+
+            $this->addFlash('danger', sprintf('No, that did not work: %s', $e->getMessage()));
+
+            $this->get('logger')->error('Yikes!', ['error' => $e->getMessage()]);
+        }
+
+        return $this->redirectToRoute('dashboard');
+    }
+
+
+
+
+    /**
+     * @Route("/upgrade-account", name="upgrade_account")
+     * @throws \InvalidArgumentException
+     * @throws \LogicException
+     */
+    public function upgradeAccountAction(UserInterface $customer)
+    {
+        try {
+            $this->get('workflow.customer_signup')->apply($customer, 'upgrade_customer');
+
+            $this->getDoctrine()->getManager()->flush();
+
+        } catch (LogicException $e) {
+
+            $this->addFlash('danger', sprintf('No, that did not work: %s', $e->getMessage()));
+
+            $this->get('logger')->error('Yikes!', ['error' => $e->getMessage()]);
         }
 
         return $this->redirectToRoute('dashboard');
